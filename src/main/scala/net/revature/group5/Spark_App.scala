3dcad5619d1e361dba88de82daa9f02e60fddbdd
Arif Banai <banai.arif@gmail.com>
2021-05-26 17:05:45 -0400

    growth stuff

--- src/main/scala/net/revature/group5/Spark_App.scala
+++ src/main/scala/net/revature/group5/Spark_App.scala
@@ -51,9 +51,13 @@ object Spark_App {
 
   def statesGrowth(): Unit = {
     dataFiles("confirmed_US").createOrReplaceTempView("confirmed_US")
-    val statesMonths = spark.sql("SELECT `Province_State`, SUM(`2/1/20`) AS Feb_2020, SUM(`3/1/20`) AS" +
-      " Mar_2020, SUM(`4/1/20`) AS Apr_2020, SUM(`5/1/20`) AS May_2020" +
-      ", SUM(`6/1/20`) AS Jun_2020 FROM confirmed_US GROUP BY `Province_State`")
+    val statesMonths = spark.sql("SELECT `Province_State`, SUM(`2/1/20`) AS Feb_2020, SUM(`3/1/20`) AS Mar_2020, " +
+      "SUM(`4/1/20`) AS Apr_2020, SUM(`5/1/20`) AS May_2020, SUM(`6/1/20`) AS June_2020, " +
+      "SUM(`7/1/20`) AS July_2020, SUM(`8/1/20`) AS August_2020, SUM(`9/1/20`) AS September_2020, " +
+      "SUM(`10/1/20`) AS October_2020, SUM(`11/1/20`) AS November_2020, SUM(`12/1/20`) AS December_2020, " +
+      "SUM(`1/1/21`) AS January_2021, SUM(`2/1/21`) AS February_2021, SUM(`3/1/21`) AS March_2021, " +
+      "SUM(`4/1/21`) AS April_2021, SUM(`5/1/21`) AS May_2021 " +
+      "FROM confirmed_US GROUP BY `Province_State`")
 
     spark.udf.register("growthPercentage", (first: Double, second: Double) => {
       if (first == 0) {
@@ -66,7 +70,20 @@ object Spark_App {
 
     statesMonths.createOrReplaceTempView("statesMonths")
 
-    val statesGrowth = spark.sql("SELECT `Province_State`, `Apr_2020`, `May_2020`, cast(growthPercentage(`Apr_2020`, `May_2020`) AS decimal(10,2)) as percent_growth FROM statesMonths")
+    val statesGrowth = spark.sql("SELECT `Province_State` AS states, cast(growthPercentage(`Apr_2020`, `May_2020`) AS decimal(10,2)) as growth_April_May, " +
+      "cast(growthPercentage(`May_2020`, `June_2020`) AS decimal(10,2)) as growth_May_June, " +
+      "cast(growthPercentage(`June_2020`, `July_2020`) AS decimal(10,2)) as growth_June_July, " +
+      "cast(growthPercentage(`July_2020`, `August_2020`) AS decimal(10,2)) as growth_July_August, " +
+      "cast(growthPercentage(`August_2020`, `September_2020`) AS decimal(10,2)) as growth_August_September, " +
+      "cast(growthPercentage(`September_2020`, `October_2020`) AS decimal(10,2)) as growth_September_October, " +
+      "cast(growthPercentage(`October_2020`, `November_2020`) AS decimal(10,2)) as growth_October_November, " +
+      "cast(growthPercentage(`November_2020`, `December_2020`) AS decimal(10,2)) as growth_November_December, " +
+      "cast(growthPercentage(`December_2020`, `January_2021`) AS decimal(10,2)) as growth_December_January, " +
+      "cast(growthPercentage(`January_2021`, `February_2021`) AS decimal(10,2)) as growth_January_February, " +
+      "cast(growthPercentage(`February_2021`, `March_2021`) AS decimal(10,2)) as growth_February_March, " +
+      "cast(growthPercentage(`March_2021`, `April_2021`) AS decimal(10,2)) as growth_March_April, " +
+      "cast(growthPercentage(`April_2021`, `May_2021`) AS decimal(10,2)) as growth_April_May " +
+      "FROM statesMonths ORDER BY states")
 
     statesGrowth.show()
   }
